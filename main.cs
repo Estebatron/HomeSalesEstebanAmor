5a01a8d2d2b7b32758c17dbc7462a0da5b449bde
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-13 22:54:28 +0000

    Made  a while loop  to try to repeat while Initial is not equal to Z but I think im going to have to end up changing to a for loop.

--- main.cs
+++ main.cs
@@ -10,10 +10,32 @@ class Program {
    Console.WriteLine ("Enter sale total");
     
       int Sale;
+      int Sale2;
+      int Sale3;
+      int Sale4;
          Sale = int.Parse(Console.ReadLine());
+  while(Initial !="Z" || Initial != "z"){
+     
+   if( Initial == "D" || Initial == "d" ||
+       Initial == "E" || Initial == "e" ||
+       Initial == "F" || Initial == "F") {
+      
+    Console.WriteLine("Input the next salesperson initial");
+           var Initial2 = Console.ReadLine();
+
+   Console.WriteLine("Input the next sale total");
+          Sale2 = int.Parse(Console.ReadLine());
+      break;
+   }
+     if( Initial == "Z" || Initial == "z"){
+        Console.WriteLine(Sale + 150);
+   break;
+     }
+     }
+     Console.WriteLine("Hey");
   
-    for ( Initial = "Z"){
+    }
+
        
     }
-  }
-}
+  
