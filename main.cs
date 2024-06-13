fd2c95f9696e6786d0ce1d819a50e8e2de4f2b93
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-13 22:12:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    
+   Console.WriteLine ("Enter salesperson initial");
+    
+      var Initial = Console.ReadLine();
+
+   Console.WriteLine ("Enter sale total");
+    
+      int Sale;
+         Sale = int.Parse(Console.ReadLine());
+  
+    for ( Initial = "Z"){
+       
+    }
+  }
+}
