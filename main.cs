2cae2edf9fda657115ecaf3c778c73c7594506c9
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-17 16:34:03 +0000

    used a do-while loop with nested if statements to  restart program if an unexcepted initial is put and stops the program if Z is put

--- main.cs
+++ main.cs
@@ -2,40 +2,75 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
+    int sale1;
+    int sale2;
+    int sale3;
+    var Initial1 = "E";
+    var Initial2 = "E";
+    var Initial3 = "E";
     
-   Console.WriteLine ("Enter salesperson initial");
-    
-      var Initial = Console.ReadLine();
-
-   Console.WriteLine ("Enter sale total");
-    
-      int Sale;
-      int Sale2;
-      int Sale3;
-      int Sale4;
-         Sale = int.Parse(Console.ReadLine());
-  while(Initial !="Z" || Initial != "z"){
+  do 
+   {
+      Console.WriteLine("Enter the first salesperson's initial");
+      Initial1 = Console.ReadLine();
      
-   if( Initial == "D" || Initial == "d" ||
-       Initial == "E" || Initial == "e" ||
-       Initial == "F" || Initial == "F") {
+      if(Initial1 != "E"|| Initial1 != "e"|| Initial1 != "F"|| Initial1 != "F"|| Initial1 != "D"|| Initial1 != "d")
+     {
+       Console.WriteLine("Invalid initial, please re-enter");
+       continue;
+     }
+      Console.WriteLine("Enter sale total");
+      sale1 = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Enter the second salesperson's initial");
+      Initial2 = Console.ReadLine();
+     
+     if(Initial2 != "E"|| Initial2 != "e"|| Initial2 != "F"|| Initial2 != "F"|| Initial2 != "D"|| Initial2 != "d"|| Initial2 != "Z" || Initial2 != "z")
+      {
+        Console.WriteLine("Invalid initial, please re-enter");
+        continue;
+      }
+     
+     if(Initial2 == "Z" || Initial2 == "z")
+     {
+       sale2 = 0;
+       sale3 = 0;
+       break;
+     }
+       Console.WriteLine("Enter the next sale total");
+       sale2 = int.Parse(Console.ReadLine());
       
-    Console.WriteLine("Input the next salesperson initial");
-           var Initial2 = Console.ReadLine();
+       Console.WriteLine("Enter the third salesperson's initial");
+        Initial3 = Console.ReadLine();
 
-   Console.WriteLine("Input the next sale total");
-          Sale2 = int.Parse(Console.ReadLine());
-      break;
+     if(Initial3 != "E"|| Initial3 != "e"|| Initial3 != "F"|| Initial3 != "F"|| Initial3 != "D"|| Initial3 != "d"|| Initial3 != "Z" || Initial3 != "z")
+     {
+       Console.WriteLine("Invalid initial, please re-enter");
+       continue;
+     }
+     
+     if(Initial3 == "Z" || Initial3 == "z")
+      {
+        sale3 = 0;
+        break;
+      }
+       Console.WriteLine("Enter the next sale total");
+       sale3 = int.Parse(Console.ReadLine());
+     break;
+     
+   }while(true);
+    Console.WriteLine("Grand Total: " + (sale1 + sale2 + sale3));
+
+    if(sale1 > sale2 && sale1 > sale3)
+    {
+      Console.WriteLine("Highest Sale:" +Initial1 );
+    }
+    else if (sale2 > sale1 && sale2 > sale3) {
+       Console.WriteLine("Highest Sale:" +Initial2 );
+    }
+    else {
+       Console.WriteLine("Highest Sale:" +Initial3 );
+    }
+    }
    }
-     if( Initial == "Z" || Initial == "z"){
-        Console.WriteLine(Sale + 150);
-   break;
-     }
-     }
-     Console.WriteLine("Hey");
-  
-    }
-
-       
-    }
   
