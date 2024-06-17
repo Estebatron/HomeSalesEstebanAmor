b3a9608a3b4eaa2d87e5d3c07d6e725a95a8d5b6
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-17 20:00:23 +0000

    Made an if statment that determines which sale was made by which person and displays their specific sale

--- main.cs
+++ main.cs
@@ -14,7 +14,7 @@ class Program {
       Console.WriteLine("Enter the first salesperson's initial");
       Initial1 = Console.ReadLine();
      
-      if(Initial1 != "E" && Initial1 != "e" && Initial1 != "D" && Initial1 != "d" && Initial1 != "F" && Initial1 != "f")
+      if(Initial1 != "E" && Initial1 != "e" && Initial1 != "D" && Initial1 != "d" && Initial1 != "F" && Initial1 !=           "f")
      {
        Console.WriteLine("Invalid initial, please re-enter");
        continue;
@@ -25,7 +25,7 @@ class Program {
       Console.WriteLine("Enter the second salesperson's initial");
       Initial2 = Console.ReadLine();
      
-     if(Initial2 != "E" && Initial2 != "e" && Initial2 != "D" && Initial2 != "d" && Initial2 != "F" && Initial2 != "f" && Initial2 != "z" && Initial2 != "Z")
+     if(Initial2 != "E" && Initial2 != "e" && Initial2 != "D" && Initial2 != "d" && Initial2 != "F" && Initial2 !=           "f" && Initial2 != "z" && Initial2 != "Z")
       {
         Console.WriteLine("Invalid initial, please re-enter");
         continue;
@@ -43,7 +43,7 @@ class Program {
        Console.WriteLine("Enter the third salesperson's initial");
         Initial3 = Console.ReadLine();
 
-     if(Initial3 != "E" && Initial3 != "e" && Initial3 != "D" && Initial3 != "d" && Initial3 != "F" && Initial3 != "f" && Initial3 != "z" && Initial3 != "Z")
+     if(Initial3 != "E" && Initial3 != "e" && Initial3 != "D" && Initial3 != "d" && Initial3 != "F" && Initial3 !=           "f" && Initial3 != "z" && Initial3 != "Z")
      {
        Console.WriteLine("Invalid initial, please re-enter");
        continue;
@@ -58,10 +58,41 @@ class Program {
        sale3 = int.Parse(Console.ReadLine());
      break;
      
-   }while(true);
+  }
+    while(true);
+    //determining which initial made Sale number 1
+    if(Initial1 == "D" || Initial1 == "d"){
+      Console.WriteLine("Danielle Total: " + sale1);
+    }
+    else if(Initial1 == "E" || Initial1 == "e"){
+      Console.WriteLine("Edward Total: " + sale1);
+    }
+    else {
+      Console.WriteLine("Francis Total: " + sale1);
+    }
+     //determining which initial made Sale number 2
+    if(Initial2 == "D" || Initial1 == "d"){
+      Console.WriteLine("Danielle Total: " + sale2);
+    }
+    else if(Initial2 == "E" || Initial1 == "e"){
+      Console.WriteLine("Edward Total: " + sale2);
+    }
+    else {
+      Console.WriteLine("Francis Total: " + sale2);
+    }
+
+     //determining which initial made Sale number 1
+    if(Initial3 == "D" || Initial3 == "d"){
+      Console.WriteLine("Danielle Total: " + sale2);
+    }
+    else if(Initial3 == "E" || Initial1 == "e"){
+      Console.WriteLine("Edward Total: " + sale3);
+    };
+    else {
+      Console.WriteLine("Francis Total: " + sale3);}
+    
     Console.WriteLine("Grand Total: " + (sale1 + sale2 + sale3));
 
-    Console.WriteLine("")
 
     if(sale1 > sale2 && sale1 > sale3)
     {
